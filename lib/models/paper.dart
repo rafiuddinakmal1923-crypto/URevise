02407c9a76da1b90a0483313fe808bb399e2a2ca
cjseven007 <hperm9166@gmail.com>
2026-03-06 22:02:40 +0800

    setup: Added folder structure and some files

---
+++ lib/models/paper.dart
@@ -0,0 +1,13 @@
+class Paper {
+  final String courseCode;
+  final String courseName;
+  final String semester;
+  final String year;
+
+  Paper({
+    required this.courseCode,
+    required this.courseName,
+    required this.semester,
+    required this.year,
+  });
+}
