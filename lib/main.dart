aa3c7a4d30afe4e14c82ac243c4d86b5e99227a3
cjseven007 <hperm9166@gmail.com>
2026-03-06 21:51:18 +0800

    fix: Moved folders and made empty application

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'URevise',
+      home: Scaffold(
+        appBar: AppBar(title: const Text('URevise')),
+        body: const Center(child: Text('Hello, World!')),
+      ),
+    );
+  }
+}
